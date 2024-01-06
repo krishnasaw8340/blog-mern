0c42f4971a617cf8a9dd0e9ed697a917f672ceb5
Krishna Saw <krishnakrsaw8340@gmail.com>
2024-01-06 10:02:03 +0530

    auth-completed

--- frontend/src/component/account/Login.jsx
+++ frontend/src/component/account/Login.jsx
@@ -17,120 +17,149 @@ const Image = styled("img")({
   // Lazy loading
   loading: "lazy",
 });
+
 const LoginButton = styled(Button) `
    text-transform: none;
    background: #8C52FF;
    color: #fff;
    border-radius: 2px;
-`
+`;
+
 const SignUpButton = styled(Button)`
-text-transform: none;
-background: white;
-color: #8C52FF;
-border-radius: 2px;
-box-shadow: 0 2px 4px 0 rgb(0 0 0 / 20%);
-`
+  text-transform: none;
+  background: white;
+  color: #8C52FF;
+  border-radius: 2px;
+  box-shadow: 0 2px 4px 0 rgb(0 0 0 / 20%);
+`;
 
 const Wrapper = styled(Box)`
   padding: 25px 35px;
   display: flex;
   flex: 1;
   flex-direction: column;
-  & > div , Button, p{
+  & > div, Button, p {
     margin-top: 20px;
   }
 `;
+
 const Text = styled(Typography)`
-   color: ##767676;
-`
+  color: ##767676;
+`;
+
 const SignUpInitialValues = {
-  name: '',
+  email: '',
   username: '',
   password: ''
 }
 
 const Login = () => {
-  const[account, toggleAccount] = useState('login');
-  const[signup, setSignup] = useState(SignUpInitialValues)
+  const [account, toggleAccount] = useState('login');
+  const [signup, setSignup] = useState(SignUpInitialValues);
   const [loginVal, setLoginVal] = useState({
-    email : "",
+    email: "",
     password: ""
   })
-  const toggleSignUp = ()=> {
-    account==='signup' ? toggleAccount('login'):toggleAccount('signup');
-  }
-  const onInputChange = (e) =>{
-    setSignup({...signup, [e.target.email]: e.target.value})
-    console.log(signup);
+
+  const toggleSignUp = () => {
+    toggleAccount(account === 'signup' ? 'login' : 'signup');
   }
 
-  const signupUser =()=>{
+  const onInputChangeSignup = (e) => {
+    setSignup({ ...signup, [e.target.name]: e.target.value });
+  }
 
+  const handleSubmitSignup = async (e) => {
+    e.preventDefault();
+    try {
+      const { data } = await axios.post(
+        "http://localhost:4000/signup",
+        {
+          ...signup
+        },
+        { withCredentials: true }
+      );
+      console.log(data);
+
+      if (data.success) {
+        alert('User successfully signed up. Please login.');
+        toggleAccount('login');
+        setSignup(SignUpInitialValues); // Clear input fields
+      } else {
+        alert('User already exists. Please login.');
+        toggleAccount('login');
+      }
+    } catch (err) {
+      console.log(err);
+    }
   }
-  const handleSubmitSignup =() => {
-    
-  }
+
   const onInputChangeLogin = (e) => {
-    const { email, password } = e.target;
+    const { name, value } = e.target;
     setLoginVal({
       ...loginVal,
-      [email]: password,
+      [name]: value,
     });
   };
-  const handleSubmitLogin =async (e) =>{
+
+  const handleSubmitLogin = async (e) => {
     e.preventDefault();
-    try{
-      const {data} = await axios.post(
+    try {
+      const { data } = await axios.post(
         "http://localhost:4000/login",
         {
           ...loginVal,
         },
-        {withCredentials: true}
+        { withCredentials: true }
       );
       console.log(data);
-    }
-    catch(err)
-    {
-      console.log(err);
 
+      if (data.success) {
+        alert('Login successful.');
+        setLoginVal({
+          email: "",
+          password: ""
+        }); // Clear input fields
+      } else {
+        alert('Incorrect Email/Password');
+      }
+    } catch (err) {
+      console.log(err);
     }
   }
-  
+
   return (
     <Component>
       <Box>
         <Box>
           <Image srcSet={`${logo} 400w`} sizes="(max-width: 600px) 100vw, 400px" alt="logo" />
         </Box>
-        {
-          account==='login'?
-          <form onSubmit={handleSubmitLogin}>
-             <Wrapper>
-          <TextField variant="standard" onChange={(e)=> onInputChangeLogin(e)} name="email "label="Enter Email" />
-          <TextField variant="standard" onChange={(e)=> onInputChangeLogin(e)} name ="password" label="Enter password" />
-          <LoginButton variant="contained">Login</LoginButton>
-          <Text variant="p" component="p" style={{textAlign: "center"}}>
-            OR
-          </Text>
-          <SignUpButton onClick={()=> toggleSignUp()}>Don't have an account?</SignUpButton>
-        </Wrapper>
-          </form>
-        :
-        <form onSubmit={handleSubmitSignup}>
-             <Wrapper>
-          <TextField variant="standard" onChange={(e)=> onInputChange(e)} name='email' label="Enter Email" />
-          <TextField variant="standard" onChange={(e)=> onInputChange(e)} name ='password' label="Enter password" />
-          <TextField variant="standard" onChange={(e)=> onInputChange(e)} name='username' label="Enter username" />
-          <SignUpButton onClick={()=>signupUser()} >SignUp</SignUpButton>
-          <Text variant="p" component="p" style={{textAlign: "center"}}>
-            OR
-          </Text>
-          <LoginButton variant="contained" onClick={()=> toggleSignUp()}>Already have an Account? </LoginButton>
-        </Wrapper>
-
-        </form>
-       
-}
+        {account === 'login' ? (
+          <Wrapper>
+            <form onSubmit={handleSubmitLogin}>
+              <TextField variant="standard" onChange={onInputChangeLogin} name="email" label="Enter Email" value={loginVal.email} />
+              <TextField variant="standard" onChange={onInputChangeLogin} name="password" label="Enter password" value={loginVal.password} />
+              <LoginButton variant="contained" type="submit">Login</LoginButton>
+              <Text variant="p" component="p" style={{ textAlign: "center" }}>
+                OR
+              </Text>
+              <SignUpButton onClick={toggleSignUp}>Don't have an account?</SignUpButton>
+            </form>
+          </Wrapper>
+        ) : (
+          <Wrapper>
+            <form onSubmit={handleSubmitSignup}>
+              <TextField variant="standard" onChange={onInputChangeSignup} name="email" label="Enter Email" value={signup.email} />
+              <TextField variant="standard" onChange={onInputChangeSignup} name="password" label="Enter password" value={signup.password} />
+              <TextField variant="standard" onChange={onInputChangeSignup} name="username" label="Enter username" value={signup.username} />
+              <SignUpButton type="submit">SignUp</SignUpButton>
+              <Text variant="p" component="p" style={{ textAlign: "center" }}>
+                OR
+              </Text>
+              <LoginButton variant="contained" onClick={toggleSignUp}>Already have an Account? </LoginButton>
+            </form>
+          </Wrapper>
+        )}
       </Box>
     </Component>
   );
