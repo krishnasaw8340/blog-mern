777c359a31f0d73c43008a83ea0ad300e5f32518
Krishna Saw <krishnakrsaw8340@gmail.com>
2024-01-06 10:25:46 +0530

    middleware-working

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
-// import { Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Login from './component/account/Login';
-// import Home from './component/Pages/Home';
+import Home from './component/Pages/Home';
 function App() {
   return (
     <div style={{marginTop:20}}>
-     <Login/>
+     {/* <Login/> */}
+     <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/auth" element = {<Login/>}/>
+     </Routes>
      
     </div>
   );
--- frontend/src/component/Pages/Home.jsx
+++ frontend/src/component/Pages/Home.jsx
@@ -1,8 +1,44 @@
 import React from 'react'
-
+import { useNavigate } from 'react-router-dom'
+import {useCookies} from "react-cookie";
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 const Home = () => {
+  const navigate = useNavigate();
+  const[cookies,removeCookie] = useCookies([]);
+  const[username, setUsername] = useState("");
+  useEffect(()=>{
+    const verifyCookie = async () =>{
+      if(!cookies.token)
+      {
+        navigate("/auth");
+      }
+      const {data}  = await axios.post(
+        "http://localhost:4000",
+        {},
+        {withCredentials:true}
+      );
+      const {status, user} = data;
+      setUsername(user);
+      return status;
+    }
+    verifyCookie();
+  },[cookies,navigate, removeCookie]);
+  const Logout = () =>{
+    removeCookie("token");
+    navigate("/auth");
+  }
   return (
-    <div>Home</div>
+    <div className="home_page">
+        <nav>
+          <section><span>MedFist</span></section>
+        </nav>
+        <h4>
+          {" "}
+          Welcome <span>{username}</span>
+        </h4>
+        <button onClick={Logout}>LOGOUT</button>
+      </div>
   )
 }
 
